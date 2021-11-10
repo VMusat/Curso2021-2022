2c19639ca18551b9bb2f68947dd1434332fa882a
VMusat <valii289@gmail.com>
2021-11-10 15:54:28 +0100

    Consulta nombreColAvanzada con wikidata y url + funcion auxiliar row + Consulta numeroPoblacion con wikidata

--- HandsOn/Group01/applicationWeb/colegiosApp/static/js/scripts.js
+++
@@ -1,6 +0,0 @@
-let btnClear = document.querySelector('button');
-let inputs = document.querySelectorAll('input');
-
-btnClear.addEventListener('click', () => {
-    inputs.forEach(input =>  input.value = '');
-});
